3c4f8ca2fadccf0108e6596a86c4b3a71d5fca97
Louis Guitton <louisguitton93@gmail.com>
2019-10-11 09:43:28 +0200

    rename folder

--- disqus-crawler/settings.py
+++ disqus-crawler/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for purseblog project
+# Scrapy settings for disqus-crawler project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,17 +9,17 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'purseblog'
+BOT_NAME = 'disqus-crawler'
 
-SPIDER_MODULES = ['purseblog.spiders']
-NEWSPIDER_MODULE = 'purseblog.spiders'
+SPIDER_MODULES = ['disqus-crawler.spiders']
+NEWSPIDER_MODULE = 'disqus-crawler.spiders'
 
 SPLASH_URL = 'http://localhost:8050/'
 DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
 HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = 'purseblog (+http://www.yourdomain.com)'
+# USER_AGENT = 'disqus-crawler (+http://www.yourdomain.com)'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS=32
@@ -27,28 +27,28 @@ HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
+# DOWNLOAD_DELAY=3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN=16
-#CONCURRENT_REQUESTS_PER_IP=16
+# CONCURRENT_REQUESTS_PER_DOMAIN=16
+# CONCURRENT_REQUESTS_PER_IP=16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED=False
+# COOKIES_ENABLED=False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED=False
+# TELNETCONSOLE_ENABLED=False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'purseblog.middlewares.MyCustomSpiderMiddleware': 543,
-#}
+# SPIDER_MIDDLEWARES = {
+#    'disqus-crawler.middlewares.MyCustomSpiderMiddleware': 543,
+# }
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
@@ -58,16 +58,16 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'purseblog.pipelines.SomePipeline': 300,
-#}
-# ITEM_PIPELINES = ['purseblog.pipelines.MongoDBPipeline', ]
+# ITEM_PIPELINES = {
+#    'disqus-crawler.pipelines.SomePipeline': 300,
+# }
+# ITEM_PIPELINES = ['disqus-crawler.pipelines.MongoDBPipeline', ]
 
 # MONGODB_SERVER = "localhost"
 # MONGODB_PORT = 27017
@@ -77,18 +77,18 @@ DOWNLOADER_MIDDLEWARES = {
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
 # NOTE: AutoThrottle will honour the standard settings for concurrency and delay
-#AUTOTHROTTLE_ENABLED=True
+# AUTOTHROTTLE_ENABLED=True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY=5
+# AUTOTHROTTLE_START_DELAY=5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY=60
+# AUTOTHROTTLE_MAX_DELAY=60
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG=False
+# AUTOTHROTTLE_DEBUG=False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED=True
-#HTTPCACHE_EXPIRATION_SECS=0
-#HTTPCACHE_DIR='httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES=[]
-#HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED=True
+# HTTPCACHE_EXPIRATION_SECS=0
+# HTTPCACHE_DIR='httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES=[]
+# HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
--- get_posts.py
+++ get_posts.py
@@ -6,9 +6,7 @@ with open("thread.json", "r+") as fo:
 
 parsed_json = json.loads(json_string)
 posts = parsed_json["response"]["posts"]
-# print len(posts)
 
-# mongoimport --db disqus --collection posts --type json --file posts.json #--jsonArray
 client = pymongo.MongoClient()
 db = client.disqus
 db.comments.insert_many(posts)
